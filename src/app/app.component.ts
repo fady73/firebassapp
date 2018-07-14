7632205cc306681c7354990ed135ddda831d9088
fady73 <fadykhayrat@gmail.com>
2018-07-14 14:26:19 +0200

    first commit

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,10 +1,55 @@
 import { Component } from '@angular/core';
 
+import { AngularFirestore, AngularFirestoreCollection, AngularFirestoreDocument } from 'angularfire2/firestore';
+
+import { Observable, of } from 'rxjs';
+import 'rxjs/add/operator/map';
+
+interface Post {
+  title: string;
+  content: string;
+}
+
+interface PostId extends Post { 
+  id: string; 
+}
+
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
   styleUrls: ['./app.component.css']
 })
+
+
 export class AppComponent {
-  title = 'app';
+  postsCol: AngularFirestoreCollection<Post>;
+  title: string;
+  content: string;
+  posts:any;
+
+
+  constructor(private afs: AngularFirestore) {
+
+  }
+
+  ngOnInit() {
+    this.postsCol = this.afs.collection('posts');
+        this.posts = this.postsCol.snapshotChanges()
+      .map(actions => {
+        return actions.map(a => {
+          const data = a.payload.doc.data() as Post;
+          const id = a.payload.doc.id;
+          return { id, data };
+        });
+      });
+
+
+  }
+  addPost() {
+    this.afs.collection('posts').add({'title': this.title, 'content': this.content});
+  }
+  deletePost(postId) {
+    this.afs.doc('posts/'+postId).delete();
+  }
+
 }
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,14 +1,34 @@
 import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
+import { AngularFireModule } from 'angularfire2';
+import { AngularFirestoreModule } from 'angularfire2/firestore';
 
 import { AppComponent } from './app.component';
+
+
+import { FormsModule } from '@angular/forms';
+
+
+// Paste in your credentials that you saved earlier
+var firebaseConfig = {
+    apiKey: "AIzaSyDwDvhedhwGGtfRdDYcXhn_Q8dp0padIqg",
+    authDomain: "angular-1012c.firebaseapp.com",
+    databaseURL: "https://angular-1012c.firebaseio.com",
+    projectId: "angular-1012c",
+    storageBucket: "angular-1012c.appspot.com",
+    messagingSenderId: "631334144567"
+};
+
 
 @NgModule({
   declarations: [
     AppComponent
   ],
   imports: [
-    BrowserModule
+    BrowserModule,
+    FormsModule,
+    AngularFireModule.initializeApp(firebaseConfig),  // Add this
+    AngularFirestoreModule 
   ],
   providers: [],
   bootstrap: [AppComponent]
